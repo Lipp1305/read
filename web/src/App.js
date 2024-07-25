1ab1b877d905bd3bbc53ba0a10380c0917de59ec
Simon Lippitt <133723073+Lipp1305@users.noreply.github.com>
2024-07-25 11:17:28 -0400

    added theme

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "@emotion/react";
 import MainPage from "./components/MainPage";
+import theme from "./themes";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <MainPage />
-    </>
+    </ThemeProvider>
   );
 }
 
